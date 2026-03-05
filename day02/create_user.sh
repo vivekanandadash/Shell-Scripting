11c98fc98e33defe776faf512991a093c7fe0571
Vivekananda <vivekanandadash245@gmail.com>
2026-03-05 19:42:17 +0000

    commit day02-varriable

---
+++ day02/create_user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Enter Username :" username
+echo "You Entered $username"
+
+sudo useradd -m $username
+
+echo "New User added"
---
+++ day02/dora.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#for fingle line comment use  ->   #
+#for multiple line comment
+<<comment
+write 
+anything here
+no issue
+comment
+name="doremon"
+echo "Name is $name , and date is $(date)"
+
+echo "enter the name:"
+read username
+echo "You entered $username"
