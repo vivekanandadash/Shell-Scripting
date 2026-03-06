af6f4db03fd44e875520a0155b549b10e230d210
Vivekananda <vivekanandadash245@gmail.com>
2026-03-06 21:35:46 +0000

    practice conditional

--- day02/conditional.sh
+++ day02/conditional.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 read -p "Are you above 18 :(yes/no)" answer
-if [:[  ]]
+if [[ $answer == "yes" ]];
+then
+	echo "you are elgible to aplly for dl"
+else
+	echo "you are not allowed"
+fi
