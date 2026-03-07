ff22c620504db3416285380393b185ea8b5fd528
Vivekananda <vivekanandadash245@gmail.com>
2026-03-07 06:35:40 +0000

    optimize conditional.sh

--- day02/conditional.sh
+++ day02/conditional.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-read -p "Are you above 18 :(yes/no)" answer
+read -p "Are you above 18 (yes/no) :" answer
 if [[ $answer == "yes" ]];
 then
 	echo "you are elgible to aplly for Driving license"
-else
+elif [[ $answer == "no" ]];
+then
 	echo "you are not allowed for Driving licence"
+else
+	echo "please enter yes or no"
 fi
