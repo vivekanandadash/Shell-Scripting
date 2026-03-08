3d5d6b9b17c74dcb028aadfc4c317fc48046bb12
Vivekananda <vivekanandadash245@gmail.com>
2026-03-08 18:35:15 +0000

    add error handling function in shell scripting

---
+++ day02/function.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This is function defination
+
+function dlChecking(){
+read -p "Are you above 18 (yes/no) :" answer
+if [[ $answer == "yes" ]];
+then
+        echo "you are elgible to aplly for Driving license"
+elif [[ $answer == "no" ]];
+then
+        echo "you are not allowed for Driving licence"
+else
+        echo "please enter yes or no"
+fi
+}
+
+#This is function calling
+dlChecking
+
---
+++ day03/errorHandling.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#echo "learn about error handling"
+create_directory() {
+mkdir demo
+}
+
+
+if ! create_directory; then
+	echo "The code is being exited as the directory already exists"
+	exit 1;
+fi
+
+echo "this should not work because the code is inturrpted"
